4a2ea01c5be5af8b24d6ab857440a231dfe8e677
Krutik6 <nkp68@ncl.ac.uk>
2022-08-14 15:52:59 +0100

    Delete ML/Current directory

--- ML/Current/HDwork/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  4 15:07:24 2022
-
-@author: nkp68
-"""
-
-__all__ = ["structuring", "fselcorr", "multiclasstest", "recfs", "featsel",
-           "kbest", "ada", "rf", "extra", "gnb", "plotbest"]
